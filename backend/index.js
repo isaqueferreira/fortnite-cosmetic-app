e748050c6704dbef9413feef3cfd4a2b38d26596
isaqueferreira <isaqueferreira916@hotmail.com>
2025-11-18 22:26:46 -0300

    feat: CreateUserController and validations

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 import 'dotenv/config'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.post('/users', async (request, response) => {
+    const createUserController = new CreateUserController()
+    const { statusCode, body } = await createUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
--- backend/src/use-cases/create-user.js
+++ backend/src/use-cases/create-user.js
@@ -1,9 +1,7 @@
-import { v4 as uuidv4 } from 'iuuid'
+import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
-import {
-    PostgresCreateUserRepository,
-    PostgresCreateUserRepository,
-} from '../repositories/postgres/create-user'
+
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
@@ -15,13 +13,15 @@ export class CreateUserUseCase {
         // inserir o usuário no banco de dados
         const user = {
             ...createUserParams,
+            vbucks_balance: 10000,
+            created_at: new Date(),
             id: userId,
             password: hashedPassword,
         }
 
         // chama o repositories
-        const PostgresCreateUserRepository = new PostgresCreateUserRepository()
-        const createdUser = await PostgresCreateUserRepository.execute(user)
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+        const createdUser = await postgresCreateUserRepository.execute(user)
 
         return createdUser
     }
